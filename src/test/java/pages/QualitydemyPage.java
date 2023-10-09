5f38d2b753676513f740c9028d843ba132d0f102
bulutluoz <abbulutluoz@gmail.com>
2023-10-09 21:24:52 +0200

    16.gun sonu

---
+++ src/test/java/pages/QualitydemyPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class QualitydemyPage {
+
+    public QualitydemyPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(linkText = "Log in")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(id = "login-email")
+    public WebElement emailKutusu;
+
+    @FindBy(id = "login-password")
+    public WebElement passwordKutusu;
+
+    @FindBy(xpath = "//button[text()='Login']")
+    public WebElement loginButonu;
+
+
+
+}
--- src/test/java/tests/day16_testNG_framework/C04_QualitydemyPozitifLogin.java
+++ src/test/java/tests/day16_testNG_framework/C04_QualitydemyPozitifLogin.java
@@ -1,6 +1,8 @@
 package tests.day16_testNG_framework;
 
 import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
 
 public class C04_QualitydemyPozitifLogin {
 
@@ -8,10 +10,16 @@ public class C04_QualitydemyPozitifLogin {
     public void pozitifLoginTesti(){
 
         //1- https://www.qualitydemy.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.qualitydemy.com/");
         //2- login linkine basin
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
         //3- Kullanici email'i olarak valid email girin
+        qualitydemyPage.emailKutusu.sendKeys("anevzatcelik@gmail.com");
         //4- Kullanici sifresi olarak valid sifre girin
+        qualitydemyPage.passwordKutusu.sendKeys("Nevzat152032");
         //5- Login butonuna basarak login olun
+        qualitydemyPage.loginButonu.click();
         //6- Basarili olarak giris yapilabildigini test edin
     }
 }
