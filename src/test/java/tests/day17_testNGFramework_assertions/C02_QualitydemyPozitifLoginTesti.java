6c2d98d0e28d92282fa780215625f5d7c7bc56ca
bulutluoz <abbulutluoz@gmail.com>
2023-10-10 20:21:22 +0200

    17.gun sonu

---
+++ src/test/java/tests/day17_testNGFramework_assertions/C02_QualitydemyPozitifLoginTesti.java
@@ -0,0 +1,31 @@
+package tests.day17_testNGFramework_assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C02_QualitydemyPozitifLoginTesti {
+
+    @Test
+    public void pozitifLoginTesti(){
+        //1- https://www.qualitydemy.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        //2- login linkine basin
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        //3- Kullanici email'i olarak valid email girin
+        qualitydemyPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecerliEmail"));
+        //4- Kullanici sifresi olarak valid sifre girin
+        qualitydemyPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+        //5- Login butonuna basarak login olun
+        qualitydemyPage.loginButonu.click();
+        //6- Basarili olarak giris yapilabildigini test edin
+
+        Assert.assertTrue(qualitydemyPage.basariliGirisElementi.isDisplayed());
+        ReusableMethods.tumSayfaFotografCek("QualitydemyPozitifLogin");
+        Driver.closeDriver();
+    }
+}
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -19,10 +22,30 @@ public class Driver {
     static WebDriver driver; // biz deger atamadigimiz icin Java default olarak null point eder
     public static WebDriver getDriver(){
 
-        WebDriverManager.chromedriver().setup();
+        String browser = ConfigReader.getProperty("browser");
 
         if (driver == null){
-            driver = new ChromeDriver();
+
+            switch (browser){
+
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver= new SafariDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver= new FirefoxDriver();
+                    break;
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
+
         }
 
         driver.manage().window().maximize();
@@ -30,6 +53,9 @@ public class Driver {
 
         return driver;
     }
+
+
+
 
 
     public static void closeDriver(){
